b6c23d47b9adf22bda73a96f0e420fba8edf8c09
Syed Behzad Hassan <behzad789@outlook.com>
2024-02-07 13:27:18 +0500

    added inital commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const axios = require("axios");
+const accountSid = "AC9f62a43da8b285ad0124e64494843a53";
+const authToken = "82f122301db56f67ac66864268471920";
+const client = require("twilio")(accountSid, authToken);
+const { Vonage } = require("@vonage/server-sdk");
+
+const vonage = new Vonage({
+  apiKey: "0b017fc1",
+  apiSecret: "BgtTOQxyXNsZDL8I",
+});
+
+const vonage2 = new Vonage({
+  apiKey: "830fdd7e",
+  apiSecret: "OZVSL3oTMzQhpqix",
+});
+
+const from = "Vonage APIs";
+const to = "923330403425";
+const to2 = "923369804391";
+const text = "Portal is Up my boy";
+
+async function sendSMS() {
+  await vonage.sms
+    .send({ to, from, text })
+    .then((resp) => {
+      console.log("Message sent successfully");
+      console.log(resp);
+    })
+    .catch((err) => {
+      console.log("There was an error sending the messages.");
+      console.error(err);
+    });
+}
+
+async function sendSMS2() {
+  await vonage2.sms
+    .send({ to2, from, text })
+    .then((resp) => {
+      console.log("Message sent successfully");
+      console.log(resp);
+    })
+    .catch((err) => {
+      console.log("There was an error sending the messages.");
+      console.error(err);
+    });
+}
+
+// Function to check website status
+async function checkWebsiteStatus(url) {
+  try {
+    const response = await axios.get(url);
+    if (response.status === 200) {
+      console.log(`Backend is ready for URL: ${url}`);
+      client.messages
+        .create({
+          body: "Portal is up courses add kar lo or agar ma soya ho to mera bhe courses add kar dena my password is 86@BDFxn or pic tujha bheje hove ha us ma sa red wala choose karna ha or jo red ka laternative ha un ko choose mat karna baki common courses ka to pata he ha",
+          from: "+12678840246",
+          to: "+923204481009",
+        })
+        .then((message) => console.log(message.sid));
+      sendSMS();
+      sendSMS2();
+    } else {
+      console.log(`Received status code ${response.status} for URL: ${url}`);
+    }
+  } catch (error) {
+    console.error(`Error fetching URL ${url}:`, error.message);
+  }
+}
+
+// URL to check
+const websiteURL = "https://online.umt.edu.pk/";
+
+// Interval in milliseconds to repeat the check
+const interval = 5000; // 5 seconds
+
+// Repeat the check at specified interval
+setInterval(() => {
+  checkWebsiteStatus(websiteURL);
+}, interval);
